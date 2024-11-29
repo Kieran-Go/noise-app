06b132027233f3cd04c822a1a5de1340c329f1a3
Kronim <kronim95@gmail.com>
2024-11-29 11:21:26 +1030

    Add more functionality
    Added a playlist feature for the vgm player, where it will
    shuffle an array of songs and play through them sequentially.
    Added more sounds.

--- js/script.js
+++ js/script.js
@@ -5,12 +5,34 @@ for(const sound of sounds){
     sound.volume = 1;
 }
 
-// Initialize the vgm audio as it is unique to the other audio
+// Initialize the vgm audio as it has additional requirements
 const vgm = document.querySelector("#vgm");
 // Change vgm song on ended
 vgm.onended = function(){
-    console.log("Song ended. Now changing songs");
+    prepareSong();
+    vgm.play();
 }
+
+const VGM_SONGS = [
+    "FF7 Remake_Midnight Rendezvous.mp3",
+    "Gran Turismo 5_Sunset Haze.mp3"
+];
+let vgmIndex = -1;
+
+function prepareSong(){
+    vgmIndex = (vgmIndex + 1) % VGM_SONGS.length; // Loop back to the start
+    vgm.setAttribute("src", "audio/vgm/" + VGM_SONGS[vgmIndex]);
+}
+
+function shuffleMusic(music) {
+    for (let i = music.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [music[i], music[j]] = [music[j], music[i]]; // Swap
+    }
+}
+
+shuffleMusic(VGM_SONGS);
+prepareSong();
 
 const volumeSliders = document.querySelectorAll(".volumeSlider");
 for(const slider of volumeSliders){
