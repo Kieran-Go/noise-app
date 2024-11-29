f5e5f1cf9b97e5d34860923ff4bfb391352b1e6a
Kronim <kronim95@gmail.com>
2024-11-29 13:41:41 +1030

    Revert "Add more programmatic song loading"
    This reverts commit 0ef72817c27d360b08cbd140cef18ac90924c338.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Plays the song on the current index
 function prepareSong(){
-    vgmIndex = (vgmIndex + 1) % vgmSongs.length; // Loop back to the start
-    vgm.setAttribute("src", "audio/vgm/" + vgmSongs[vgmIndex]);
+    vgmIndex = (vgmIndex + 1) % VGM_SONGS.length; // Loop back to the start
+    vgm.setAttribute("src", "audio/vgm/" + VGM_SONGS[vgmIndex]);
 }
 
 // Randomly shuffles the order of an array of music
@@ -23,34 +23,15 @@ vgm.onended = function(){
     vgm.play();
 }
 
-// Get an array of every song by reading from a txt file
-async function loadSongs() {
-    const vgmPath = "../audio/vgm/songs.txt";
-    let vgmSongs;
-
-    try {
-        const response = await fetch(vgmPath);
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const text = await response.text();
-        vgmSongs = text.split('\n').map(song => song.trim()).filter(line => line);
-    } catch (error) {
-        console.error('Error fetching the file:', error);
-    }
-    return vgmSongs;
-}
-
-// Initialize the songs array
-let vgmSongs = [];
+// VGM songs array
+const VGM_SONGS = [
+    "FF7 Remake_Midnight Rendezvous.mp3",
+    "Gran Turismo 5_Sunset Haze.mp3"
+];
 let vgmIndex = -1;
 
-// Load, shuffle and prepare the songs
-loadSongs().then(songsArray => {
-    vgmSongs = songsArray;
-    shuffleMusic(vgmSongs);
-    prepareSong();
-});
+shuffleMusic(VGM_SONGS);
+prepareSong();
 
 // Initialize the volume sliders
 const volumeSliders = document.querySelectorAll(".volumeSlider");
